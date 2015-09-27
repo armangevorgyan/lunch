5bcc16e4e7c815f0a39ae94606c0ede301689b33
Hayk <hayk.hovhannisyan@sflpro.com>
2015-09-27 11:13:10 +0400

    Add post and delete to abstract web service.

--- src/static/scripts/webServices/AbstractWebService.js
+++ src/static/scripts/webServices/AbstractWebService.js
@@ -4,10 +4,16 @@
     AbstreactWebService.prototype.constructor = function($q, $http) {
         this.$q = $q;
         this.$http = $http;
+        this.url = 'http://10.18.1.67:8080';
+        this.options = {
+            headers: {
+                Authorization: '29'
+            }
+        };
     };
     AbstreactWebService.prototype.get = function(url) {
         var defer = this.$q.defer();
-        this.$http.get(url)
+        this.$http.get(this.url + url, this.options)
             .then(function(response) {
                 defer.resolve(response.data);
             })
@@ -22,7 +28,37 @@
     };
     AbstreactWebService.prototype.put = function(url, body) {
         var defer = this.$q.defer();
-        this.$http.put(url, body)
+        this.$http.put(this.url + url, body, this.options)
+            .then(function(response) {
+                defer.resolve(response.data);
+            })
+            .catch(function(response) {
+                console.error('[AbstractWebService] Request:', url, 'Response:', response.status, response.data);
+                defer.reject({
+                    status: response.status,
+                    data: response.data
+                });
+            });
+        return defer.promise;
+    };
+    AbstreactWebService.prototype.post = function(url, body) {
+        var defer = this.$q.defer();
+        this.$http.post(this.url + url, body, this.options)
+            .then(function(response) {
+                defer.resolve(response.data);
+            })
+            .catch(function(response) {
+                console.error('[AbstractWebService] Request:', url, 'Response:', response.status, response.data);
+                defer.reject({
+                    status: response.status,
+                    data: response.data
+                });
+            });
+        return defer.promise;
+    };
+    AbstreactWebService.prototype.delete = function(url, body) {
+        var defer = this.$q.defer();
+        this.$http.delete(this.url + url, body, this.options)
             .then(function(response) {
                 defer.resolve(response.data);
             })
